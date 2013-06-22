8365d756de208423eea2b1cfd19bfc519ecb1418
mimming <jen@mimming.com>
2013-06-22 14:33:14 -0700

    a really crude html wireframe

---
+++ upload.php
@@ -0,0 +1,28 @@
+<?php
+$happy = false;
+$sad = false;
+
+# get the image
+
+# do any necessary processing
+
+# hit the API with curl
+
+# get json back
+
+# render the happy / sad / angry cow page
+if($happy) {
+  echo "happy cow!";
+} else if ($sad) {
+  echo "Sad cow!";
+} else {
+  echo "unknown cow!";
+}
+
+# Render detailed attributes
+?>
+<ul id="details">
+  <li>Attribute 1: value</li>
+  <li>Attribute 2: value</li>
+  <li>Attribute 3: value</li>
+</ul>
