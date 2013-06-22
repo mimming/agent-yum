f29ab919c2cf33e08d46b8b426d628d3510a32a6
mimming <jen@mimming.com>
2013-06-22 15:05:05 -0700

    does an api call

--- upload.php
+++ upload.php
@@ -1,14 +1,56 @@
 <?php
+# add image part to a kooaba multipart request
+function image_part($boundary, $attr_name, $file_name, $data) {
+  $str  = "--" . $boundary . "\r\n";
+  $str .= 'Content-Disposition: form-data; name="'. $attr_name .'"; filename="' . $file_name . '"' . "\r\n";
+  $str .= 'Content-Transfer-Encoding: binary' ."\r\n";
+  $str .= 'Content-Type: image/jpeg' . "\r\n\r\n";
+  $str .= $data . "\r\n";
+  return $str;
+}
+
+# add text part to a kooaba multipart request
+function text_part($boundary, $attr_name, $data) {
+  $str = "--" . $boundary . "\r\n";
+  $str .= 'Content-Disposition: form-data; name="'. $attr_name .'"'."\r\n\r\n";
+  $str .= $data . "\r\n";
+  return $str;
+}
+
 $happy = false;
 $sad = false;
 
 # get the image
+$photo = $_FILES["image"];
+
+$photo_bytes = file_get_contents($photo["tmp_name"]);
 
 # do any necessary processing
 
 # hit the API with curl
+# curl -X POST -F image=@glass-close.jpg
+#  -H 'Authorization: Token WE27xAaZUCoD7R2dDENPeGdyzploQBKVwRM6Qkee'
+#  -H 'Accept: application/json' https://query-api.kooaba.com/v4/query
 
-# get json back
+$boundary = uniqid();
+
+# Construct the body of the request
+$body  = image_part($boundary, "image", "query_image.jpg", $photo_bytes);
+$body .= "--" . $boundary . "--\r\n";
+
+$context = stream_context_create(array(
+  'http' => array(
+    'method' => 'POST',
+    'header' => 'Content-type: multipart/form-data; boundary='.$boundary."\r\n" .
+    'Authorization: Token WE27xAaZUCoD7R2dDENPeGdyzploQBKVwRM6Qkee',
+    'content' => $body
+  )
+));
+
+$result = file_get_contents("https://query-api.kooaba.com/v4/query", false, $context);
+
+echo "Result: ", $result;
+# parse response
 
 # render the happy / sad / angry cow page
 if($happy) {
